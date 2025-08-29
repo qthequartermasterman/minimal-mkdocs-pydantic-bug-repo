79b0b164c55d825e0dce7c5d94dce2bcf1ca6634
Andrew Sansom <andrew@protopia.ai>
2025-08-29 16:04:49 -0500

    docs: add reproducibility instructions

--- src/pydantic_settings_mre/settings.py
+++ src/pydantic_settings_mre/settings.py
@@ -1,4 +1,11 @@
 import pydantic_settings
 
+class MyModel(pydantic.BaseModel):
+    id: int
+    name: str
+
+class AnotherModel(MyModel):
+    asdf: str
+
 class MySettingsModel(pydantic_settings.BaseSettings):
     asdf: str
